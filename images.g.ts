e51af91c8eef43205dd4b29aebd11f7a2005ee87
FIVECOOKIETOWN <107253207+FIVECOOKIETOWN@users.noreply.github.com>
2022-06-14 14:53:19 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Player":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 6 6 f f f . . . . 
+. . . f f f 6 6 6 6 f f f . . . 
+. . f f f 6 6 6 6 6 6 f f f . . 
+. . f f 6 9 9 9 9 9 9 6 6 f . . 
+. . f 6 9 f f f f f f 9 6 f . . 
+. . f f f f 6 6 6 6 f f f f . . 
+. f f 6 f b f 4 4 f b f 6 f f . 
+. f 6 6 4 1 f 4 4 f 1 4 6 6 f . 
+. . f 6 6 4 4 4 4 4 4 6 6 f . . 
+. . . f 6 6 4 f f 4 6 6 f . . . 
+. . 6 4 f 9 9 9 9 9 9 f 4 6 . . 
+. . 4 d f 9 9 9 9 9 9 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image2":
+            case "PlayerRight":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . f f 6 6 6 6 6 6 f . . . 
+. . . f f 6 6 6 6 6 6 6 6 f . . 
+. . . f 6 6 6 6 f f f f f f . . 
+. . . f f f f f f 9 9 9 9 6 f . 
+. . . f 6 9 9 9 f f f f 6 9 f . 
+. . f f f f f f f 6 6 6 f f f . 
+. . f f 6 4 4 6 b f 4 4 6 6 f . 
+. . f 6 6 4 d 4 1 f 4 4 6 f . . 
+. . . f 6 6 6 4 4 4 f f 6 f . . 
+. . . . 4 d d 6 4 4 4 6 f . . . 
+. . . . 6 d d 6 9 9 9 9 f . . . 
+. . . . f 6 6 f 4 4 5 5 f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . . f f . . . f f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,21 +1,71 @@
-tiles.setCurrentTilemap(tilemap`level1`)
-let mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 9 9 f f f . . . . 
-    . . . f f f 9 9 9 9 f f f . . . 
-    . . f f f 6 6 6 6 6 6 f f f . . 
-    . . f f 6 9 9 9 9 9 9 6 6 f . . 
-    . . f 6 9 f f f f f f 9 6 f . . 
-    . . f f f f 6 6 6 6 f f f f . . 
-    . f f 6 f b f 4 4 f b f 6 f f . 
-    . f 6 6 4 1 f d d f 1 4 6 6 f . 
-    . . f 6 6 d d d d d d 6 6 f . . 
-    . . . f 6 6 4 4 4 4 6 6 f . . . 
-    . . 6 4 f 9 9 9 9 9 9 f 4 6 . . 
-    . . 4 d f 9 9 9 9 9 9 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 6 6 6 6 f f . . . . 
+        . . . f 6 6 6 f f 6 6 6 f . . . 
+        . . . f f f f 9 9 f f f f . . . 
+        . . f f 6 9 6 9 9 6 9 6 f f . . 
+        . . f 6 9 f 9 f f f 9 f 6 f . . 
+        . . f f f 9 f 6 6 9 9 f f f . . 
+        . . f 6 9 f f 6 6 9 f 6 6 f . . 
+        . f f 6 f f 6 6 6 f 6 6 6 f f . 
+        . f f 6 6 6 6 6 6 6 6 6 6 f f . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . . 6 f f f f f f f f 4 6 . . 
+        . . . 4 f 9 9 9 9 9 6 d d 4 . . 
+        . . . 6 f f f f f f 6 6 4 . . . 
+        . . . . f f f . . . . . . . . . 
+        `)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 6 6 6 6 6 6 f f . . . . 
+        . . f 6 6 6 6 6 6 6 6 f f . . . 
+        . . f f f f f 6 6 6 6 6 f . . . 
+        . f 6 9 9 9 9 f f f f f f . . . 
+        . f 9 6 f f f f 9 9 9 6 f . . . 
+        . f f f 6 6 6 f f f f f f f . . 
+        . f 6 6 4 4 f b 6 4 4 6 f f . . 
+        . . f 6 4 4 f 1 4 d 4 6 6 f . . 
+        . . f 6 f f 4 6 6 6 6 6 f . . . 
+        . . . f 6 4 6 d d 4 f . . . . . 
+        . . . f 9 9 6 d d 6 f . . . . . 
+        . . f f 5 5 f 6 6 f f f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`PlayerRight`)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 6 6 6 6 f f . . . . 
+        . . . f 6 6 6 6 6 6 6 6 f . . . 
+        . . f f f f f f f f f f f f . . 
+        . . f 6 6 9 9 9 9 9 9 6 f f . . 
+        . f f 6 9 f f f f f f 9 6 f f . 
+        . f f f f f 6 6 6 6 f f f f f . 
+        . . f 6 f b f 4 4 f b f 6 f . . 
+        . . f 6 4 1 f 4 4 f 1 4 6 f . . 
+        . . 6 f 6 4 4 f f 4 4 6 f . . . 
+        . . 6 4 d d 6 6 6 6 6 f 6 f . . 
+        . . . 6 d d 6 6 6 6 6 f 4 6 . . 
+        . . . . 6 6 f 5 5 4 4 f . . . . 
+        . . . . . f f f f f f f . . . . 
+        . . . . . . . . . f f f . . . . 
+        `)
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`PathToPurpleRoom`)
+mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+forever(function () {
+    music.playMelody("C D F D F D F G ", 120)
+})
